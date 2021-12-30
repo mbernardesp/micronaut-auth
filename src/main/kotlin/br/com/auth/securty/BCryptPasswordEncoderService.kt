852b7ad1195db8c1cb42168ae196135144c7e2fb
Marcelo <marcelobernardespinto@gmail.com>
2021-12-30 11:31:48 -0300

    Adicionada classe para criptografia por bcrypt

--- src/main/kotlin/br/com/auth/securty/BCryptPasswordEncoderService.kt
+++ src/main/kotlin/br/com/auth/securty/BCryptPasswordEncoderService.kt
@@ -1,4 +1,19 @@
 package br.com.auth.securty
 
-class BCryptPassword {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import javax.inject.Singleton
+
+@Singleton
+class BCryptPasswordEncoderService : PasswordEncoder {
+
+    private val bCryptEncoder = BCryptPasswordEncoder()
+
+    override fun encode(rawPassword: CharSequence?): String {
+        return bCryptEncoder.encode(rawPassword)
+    }
+
+    override fun matches(rawPassword: CharSequence?, encodedPassword: String?): Boolean {
+        return bCryptEncoder.matches(rawPassword, encodedPassword)
+    }
 }
